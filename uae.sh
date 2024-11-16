c4a1776522350758d8782fc371a9199b8e789b86
gennibot5740 <adampries360@gmail.com>
2024-11-16 07:24:55 -0800

    Add files via upload

---
+++ uae.sh
@@ -0,0 +1,7 @@
+echo "# iBORGSCyberBARn" >> README.md
+git init
+git add README.md
+git commit -m "/home/www/unitedmachine.uk:443"
+git branch -M main
+git remote add origin https://github.com/gennibot5740/iBORGSCyberBARn.git
+git push -u origin main
